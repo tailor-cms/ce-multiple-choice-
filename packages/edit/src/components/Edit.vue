46224b7e1b3bda3d0142392c3c1e42fe3ab7007e
rkusan00 <renato.kusanovic@gmail.com>
2024-12-16 10:13:17 +0100

    Refactor component to use isGradable prop

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -1,23 +1,24 @@
 <template>
   <QuestionContainer
     v-bind="{
-      allowedEmbedTypes,
+      type: manifest.name,
+      icon: manifest.ui.icon,
+      embedTypes,
       elementData,
       isDirty,
       isDisabled,
-      isGradeable,
     }"
     show-feedback
     @cancel="updateData(element.data)"
     @save="save"
     @update="updateData($event)"
   >
-    <div class="text-subtitle-2 mb-2">{{ title }}</div>
     <VInput
       v-slot="{ isValid }"
       :model-value="elementData.correct"
       :rules="validation.correct"
     >
+      <div class="text-subtitle-2 mb-2">{{ title }}</div>
       <VSlideYTransition group>
         <VTextField
           v-for="(answer, index) in elementData.answers"
@@ -32,7 +33,7 @@
         >
           <template #prepend>
             <VCheckbox
-              v-if="isGradeable"
+              v-if="isGradable"
               v-model="elementData.correct"
               :error="isValid.value === false"
               :readonly="isDisabled"
@@ -66,7 +67,7 @@
         </VTextField>
       </VSlideYTransition>
     </VInput>
-    <div class="d-flex justify-end mb-12">
+    <div class="d-flex justify-end mb-4">
       <VBtn
         v-if="!isDisabled"
         color="primary-darken-4"
@@ -83,38 +84,41 @@
 
 <script lang="ts" setup>
 import { computed, defineEmits, defineProps, reactive, watch } from 'vue';
-import { Element, ElementData } from '@tailor-cms/ce-multiple-choice-manifest';
+import manifest, {
+  Element,
+  ElementData,
+} from '@tailor-cms/ce-multiple-choice-manifest';
 import cloneDeep from 'lodash/cloneDeep';
 import isEqual from 'lodash/isEqual';
 import { QuestionContainer } from '@tailor-cms/core-components';
 
 const props = defineProps<{
-  allowedEmbedTypes: string[];
+  embedTypes: any[];
   element: Element;
   isFocused: boolean;
   isDisabled: boolean;
-  isGradeable: boolean;
 }>();
 const emit = defineEmits(['save']);
 
 const elementData = reactive<ElementData>(cloneDeep(props.element.data));
 
+const isGradable = computed(() => props.element.data.isGradable);
 const answersCount = computed(() => elementData.answers.length);
 const isDirty = computed(() => !isEqual(elementData, props.element.data));
 
 const title = computed(() =>
-  props.isGradeable ? 'Select correct answer(s)' : 'Options',
+  isGradable.value ? 'Select correct answer(s)' : 'Options',
 );
 const placeholder = computed(() =>
-  props.isGradeable ? 'Answer...' : 'Option...',
+  isGradable.value ? 'Answer...' : 'Option...',
 );
 const btnLabel = computed(() =>
-  props.isGradeable ? 'Add answer' : 'Add option',
+  isGradable.value ? 'Add answer' : 'Add option',
 );
 
 const validation = computed(() => ({
   answer: [(val: string) => !!val || 'Answer is required'],
-  correct: props.isGradeable
+  correct: isGradable.value
     ? [(val?: number[]) => val?.length || 'Please choose the correct answer(s)']
     : [],
 }));
@@ -123,7 +127,7 @@ const addAnswer = () => elementData.answers.push('');
 const removeAnswer = (answerIndex: number) => {
   elementData.answers.splice(answerIndex, 1);
 
-  if (props.isGradeable) {
+  if (isGradable.value) {
     const index = elementData.correct!.indexOf(answerIndex);
     if (index !== -1) elementData.correct!.splice(index, 1);
     elementData.correct!.forEach((it, i) => {
